3e684cfb2675552a97dc8405fa430094275b4fce
sylluhuk <you@example.com>
2022-12-02 05:19:50 -0500

    The idea has been done

--- laba3.java
+++ laba3.java
@@ -5,45 +5,45 @@ import java.io.RandomAccessFile;
 
 interface Animal
 {
-
-	void animalsNum (); 
-	void printer (double age);
+	void animalsNum(); 
+	void printer(double age);
 	void fodderCounter(double age, double weight);
-	void randomizer(int ageFactor, int weightFactor)
-		
+	void randomizer(int ageFactor, int weightFactor);
 }
 
 class Dog implements Animal
 {
-	double ageDog;
-	double weightDog;
+	double age;
+	double weight;
 	double quantityFodder;
 	int weightFactor = 110;
 	int ageFactor = 17;
 	String dogN = "Dog";
+
 	ArrayList<Object> animalsList = new ArrayList<Object>();
 
 	@Override
 	public void fodderCounter(double age, double weight)
 	{
-		quantityFodder = age <= 0.5 ? weight*0.07 : weightDog*0.035;  
+		quantityFodder = age <= 0.5 ? weight*0.07 : weight*0.035;  
 	}
-	
+
+	@Override
+	public void randomizer(int ageFactor, int weightFactor)
+	{
+		weight = (Math.random()*weightFactor);
+		age = (Math.random()*ageFactor);
+	}
+
+	@Override
 	public void animalsNum()
 	{ 	
-		public void randomizer(int ageFactor, int weightFactor)
-		{
-			weight = (Math.random()*weightFactor);
-			age = (Math.random()*ageFactor);
-		}
-
 		animalsList.add(dogN); 
-		animalsList.add(ageDog);
-		animalsList.add(weightDog);
-	
+		animalsList.add(age);
+		animalsList.add(weight);
 	}
 
-	public void printer(ageDog)
+	public void printer(double ageDog)
 	{
 		System.out.println(ageDog);
 	}
@@ -51,10 +51,9 @@ class Dog implements Animal
 
 class SearchInfo
 {
-	Dog dog = new Dog();
-	public void printInfo ()
+	public void printInfo (ArrayList info)
 	{
-		System.out.println("Info about dog: "+dog.animalsList);
+		System.out.println("Info about dog: "+info);
 	
 	}
 }
@@ -64,10 +63,10 @@ class laba2
 	public static void main(String[] args)
 	{
 		Dog dog = new Dog();
+		dog.randomizer(dog.ageFactor, dog.weightFactor);
+		dog.animalsNum();
 		SearchInfo info = new SearchInfo();
-			
-		info.printInfo();
-		dog.printer();
+		info.printInfo(dog.animalsList);
 	}
 }
 
