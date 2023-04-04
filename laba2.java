983c44f75854394f2fecfa039c584821f7679a42
sullihuk <nyxa@nextmail.ru>
2023-04-04 05:59:37 -0400

    It eventually works

--- laba2.java
+++ laba2.java
@@ -1,66 +1,75 @@
 import java.lang.Math;
+import java.text.NumberFormat;
 import java.util.*;
 
 
 interface Animal // Инициализация интерфейса 
 {
 
-	void printer(int number);
-  void fodderCounter();
-
+	void printer();
+  void fodderCounter(String name, int number, int weightFactor, int ageFactor, double youngFactor, double  youngPortionFactor, double adultPortionFactor);
  }
 
 class Dog implements Animal
 {
-  double quantityFodder;
-  double puppyKittyFactor = 0.5;
-  double puppyKittyPortionFactor = 0.07;
-  double adultPortionFactor = 0.035;
-	double age;
-	double weight;
-	int weightFactor = 110;
-	int ageFactor = 17;
-	String name = "Dog_";
-	int number = 1;
+  double quantityFodder; //Расчетное количество корма
+  double puppyFactor = 0.5; //Возраст псины, до которого она считается щенком 
+  double puppyPortionFactor = 0.07; // Порция для щенка на кг веса
+  double adultPortionFactorD = 0.035; // Порция для взрослого собакентия
+	double age; //Возраст животного
+	double weight; //Вес/масса животного
+	int weightFactorD = 110; // Максимальный вес собакeнтиев
+	int ageFactorD = 17;// Максимальный возраст собак
+	String name = "Dog_";// Шаблон имени собакентия
+
 
 	ArrayList<Object> animalsList = new ArrayList<Object>();// Объявление массива, который будет содержать информацию о животных: вид животного с порядковым номером; возраст; массу; расчетное количество корма
 
-  public void randomizer(int ageFactor, int weightFactor)
-	{
-		weight = (Math.random()*weightFactor);
-		age = (Math.random()*ageFactor);
-	}
-
-  public void fodderCounter()//Метод расчитывает количество корма для животного. Не совсем в соответсвии с заданием т.к. нет принимаемых параметров
+  @Override
+  public void fodderCounter(String name, int number, int weightFactor, int ageFactor , double youngFactor, double  youngPortionFactor, double adultPortionFactor)//Метод расчитывает количество корма для животного. Не совсем в соответсвии с заданием, т.к. принимаемых параметров больше чем по заданию, однако, они необходимы для генерации экземпляров соответствующих классов 
     {
-      quantityFodder = age <= puppyKittyFactor ? weight*puppyKittyPortionFactor : weight*adultPortionFactor;  
+      weight = (Math.random()*weightFactor);//Генератор веса животных
+		  age = (Math.random()*ageFactor); //Генератор возраста животных
+
+      quantityFodder = age <= youngFactor ? weight*youngPortionFactor : weight*adultPortionFactor;//Тернарный оператор для расчета количества корма для щенка/котенка или взрослой особи 
+  
+
+      animalsList.add(name+number); 
+      animalsList.add(weight);
+      animalsList.add(age);
+      animalsList.add(quantityFodder);
     }
 
-	public void animalsNum()// Метод заполняет массив данными о животине.
-	{ 	
-		animalsList.add(name+number); 
-		animalsList.add(age);
-		animalsList.add(weight);
-		animalsList.add(quantityFodder);
-	}
-
-	public void printer(int number)
+  @Override
+	public void printer()
 	{
+    NumberFormat form = NumberFormat.getInstance();
+
+
     for(int i = 0; i < animalsList.size(); i++)
     {
-		System.out.print(animalsList.get(i) + " ");
+      if (i == 0){
+        System.out.print(animalsList.get(i) + "\t");
+      } else if (i != animalsList.size()-1) {
+        form.setMaximumFractionDigits(1);
+        System.out.print(form.format(animalsList.get(i)) + "\t");
+      }
+      else {
+        form.setMaximumFractionDigits(3);
+        System.out.print(form.format(animalsList.get(i)) + "\t\t");
+      } 
     }
 	}
 }
 
 class Cat extends Dog implements Animal  
 {
-  double puppyKittyFactor = 1;
-  double puppyKittyPortionFactor = 0.017;
-  double adultPortionFactor = 0.011;
-	int weightFactor = 20;
-	int ageFactor = 18;
-	String name = "Cat_";
+  double kittyFactor = 1;
+  double kittyPortionFactor = 0.017;
+  double adultPortionFactorC = 0.011;
+	int weightFactorC = 20;
+	int ageFactorC = 18;
+	String nameC = "Cat_";
 
 
 }
@@ -117,11 +126,16 @@ class laba2
 {
 	public static void main(String[] args)
 	{
-    Dog dog = new Dog();
-    dog.randomizer(dog.ageFactor, dog.weightFactor);
-    dog.fodderCounter();
-    dog.animalsNum();
-    dog.printer(10);
+    for (int i = 0; i < 100; i++) {
+   
+      Dog dog = new Dog();
+      Cat cat = new Cat();
 
+      dog.fodderCounter(dog.name, i+1, dog.weightFactorD, dog.ageFactorD, dog.puppyFactor, dog.puppyPortionFactor, dog.adultPortionFactorD);
+      cat.fodderCounter(cat.nameC, i+1, cat.weightFactorC, cat.ageFactorC, cat.kittyFactor, cat.kittyPortionFactor, cat.adultPortionFactorC );
+      dog.printer();
+      cat.printer();
+      System.out.println();
+    }
 	}
 }
