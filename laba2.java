e2117a76092a440e110233d6a415ab70761f3030
sullihuk <nyxa@nextmail.ru>
2023-04-18 06:06:01 -0400

    Starting again

--- laba2.java
+++ laba2.java
@@ -5,8 +5,9 @@ import java.util.*;
 
 interface Animal // Инициализация интерфейса 
 {
-	void printer(double[] list, String name, int number);
-  void fodderCounter(int weightFactor, int ageFactor, double youngFactor, double  youngPortionFactor, double adultPortionFactor);
+
+	void printer(ArrayList list);
+  void fodderCounter(String name, int number, int weightFactor, int ageFactor, double youngFactor, double  youngPortionFactor, double adultPortionFactor);
  }
 
 class Dog implements Animal
@@ -22,41 +23,39 @@ class Dog implements Animal
 	String name = "Dog_";// Шаблон имени собакентия
 
 
-  double[] animalsList = new double[3];
-//	ArrayList<Object> animalsList = new ArrayList<>();// Объявление массива, который будет содержать информацию о животных: вид животного с порядковым номером; возраст; массу; расчетное количество корма
+	ArrayList<Object> animalsList = new ArrayList<>();// Объявление массива, который будет содержать информацию о животных: вид животного с порядковым номером; возраст; массу; расчетное количество корма
 
   @Override
-  public void fodderCounter(int weightFactor, int ageFactor , double youngFactor, double  youngPortionFactor, double adultPortionFactor)//Метод расчитывает количество корма для животного. Не совсем в соответсвии с заданием, т.к. принимаемых параметров больше чем по заданию, однако, они необходимы для генерации экземпляров соответствующих классов 
+  public void fodderCounter(String name, int number, int weightFactor, int ageFactor , double youngFactor, double  youngPortionFactor, double adultPortionFactor)//Метод расчитывает количество корма для животного. Не совсем в соответсвии с заданием, т.к. принимаемых параметров больше чем по заданию, однако, они необходимы для генерации экземпляров соответствующих классов 
     {
       weight = (Math.random()*weightFactor);//Генератор веса животных
 		  age = (Math.random()*ageFactor); //Генератор возраста животных
 
       quantityFodder = age <= youngFactor ? weight*youngPortionFactor : weight*adultPortionFactor;//Тернарный оператор для расчета количества корма для щенка/котенка или взрослой особи 
+  
 
-      /*animalsList.add(name+number); 
+      animalsList.add(name+number); 
       animalsList.add(weight);
       animalsList.add(age);
-      animalsList.add(quantityFodder);*/
-      animalsList[0] = weight;
-      animalsList[1] = age;
-      animalsList[2] = quantityFodder;
+      animalsList.add(quantityFodder);
     }
 
   @Override
-	public void printer(double[] list, String name, int number)
+	public void printer(ArrayList list)
 	{
     NumberFormat form = NumberFormat.getInstance();
 
-    System.out.print(name + number + "\t");
-    for(int i = 0; i < list.length; i++)
+    for(int i = 0; i < list.size(); i++)
     {
-      if (i == 0 || i == 1) {
+      if (i == 0){
+        System.out.print(list.get(i) + "\t");
+      } else if (i != list.size()-1) {
         form.setMaximumFractionDigits(1);
-        System.out.print(form.format(list[i]) + "\t");
+        System.out.print(form.format(list.get(i)) + "\t");
       }
       else {
         form.setMaximumFractionDigits(3);
-        System.out.print(form.format(list[i]) + "\t\t");
+        System.out.print(form.format(list.get(i)) + "\t\t");
       } 
     }
 	}
@@ -76,8 +75,8 @@ class SearchInfo
 {
   int quantityObjects = 100;
 	double maxPortionG = 0;
-  HashMap<int, double> listOfObjectsD = new HashMap<>();
-  HashMap<int, double> listOfObjectsC = new HashMap<>();
+  ArrayList<Object> listOfObjectsD = new ArrayList<>();
+  ArrayList<Object> listOfObjectsC = new ArrayList<>();
   
   Dog dog = new Dog();
   Cat cat = new Cat();
@@ -86,44 +85,39 @@ class SearchInfo
 	{
 		System.out.println(" To see portions of dogs which gobbled up more than others press (D).\n.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.\n To see portions of cats which gobbled up more than others press (C).\n.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.\n");
 		Scanner input = new Scanner(System.in);
-    char oneOfAnimals = input.next().charAt(0);
-    while (oneOfAnimals != 'c' || oneOfAnimals   != 'C' || oneOfAnimals != 'd' || oneOfAnimals != 'D') {
-      input.next().charAt(0);
+		char oneOfAnimals = input.next().charAt(0);
+    while (oneOfAnimals != 'c' || oneOfAnimals != 'd' || oneOfAnimals != 'C' || oneOfAnimals != 'D') 
       oneOfAnimals = input.next().charAt(0);
-    }
 		return oneOfAnimals;
 	}
 
 	public double scannerValue(char choice)
 	{
-		Scanner inputV = new Scanner(System.in);
-		double fodderQuantity = inputV.nextDouble(); 
-    if (choice == 'c' || choice == 'C')
-		  System.out.println(" Julia please enter desired value of fodder for"+cat.name.substring(0,2)+": ");
-    if (choice == 'd' || choice == 'D'){
-      System.out.println(" Julia please enter desired value of fodder for"+dog.name.substring(0,2)+ "from "+ dog.adultPortionFactorD*0.1+"up to " + dog.puppyPortionFactor*dog.weightFactorD+ ": ");
-		  fodderQuantity = inputV.nextDouble();
-      while (Double.valueOf(fodderQuantity).getClass().getName() != "Double" && (fodderQuantity < dog.puppyPortionFactor * 0.1 || fodderQuantity > dog.puppyPortionFactor * dog.weightFactorD)) // Условие ввода значения пользователем. Здесь подразумевается, что минимальная порция это - порция щенка уменьшенная в десять раз.
-      {
-        System.out.println(" Julia please enter a correct value of fodder for"+dog.name.substring(0,2)+": ");
-        fodderQuantity = inputV.nextDouble();
-      }
-    }
+		System.out.println(" Julia please enter desired value of fodder: ");
+		Scanner input = new Scanner(System.in);
+		double fodderQuantity = input.nextDouble();
+    if (choice == 'd' || choice == 'D')
+      while (!(fodderQuantity instanceof Double) || fodderQuantity <dog.adultPortionFactorD*0.1 || fodderQuantity > dog.puppyPortionFactor*dog.weightFactorD )  
+        fodderQuantity = input.nextDouble();
+
 		return fodderQuantity;
 	}
 
-	public void printerInfo(String name, HashMap list, double fodderQuantity)
+	public void printerInfo(ArrayList list, double fodderQuantity)
 	{
     NumberFormat form = NumberFormat.getInstance();
 
-    for(Map.Entry<Integer, Double> e : list.entrySet())
+    for(int i = 0; i < list.size(); i++)
     {
+      if (i%2 == 0)
+        System.out.print(list.get(i) + "\t");
+      else {
         form.setMaximumFractionDigits(3);
-        System.out.print(name + e.getKey() + "\t" + form.format(e.getValue()) + "\t\t");
+        System.out.print(form.format(list.get(i)) + "\t\t");
         System.out.println();
       }
     }
-  
+  }
     
 	public void groupOfObjects()
 	{
@@ -132,13 +126,15 @@ class SearchInfo
       Dog dog = new Dog();
       Cat cat = new Cat();
 
-      dog.fodderCounter(dog.weightFactorD, dog.ageFactorD, dog.puppyFactor, dog.puppyPortionFactor, dog.adultPortionFactorD);
-      listOfObjectsD.put(i, dog.animalsList[2]);
-      cat.fodderCounter(cat.weightFactorC, cat.ageFactorC, cat.kittyFactor, cat.kittyPortionFactor, cat.adultPortionFactorC);
-      listOfObjectsC.put(i, cat.animalsList[2]);
+      dog.fodderCounter(dog.name, i+1, dog.weightFactorD, dog.ageFactorD, dog.puppyFactor, dog.puppyPortionFactor, dog.adultPortionFactorD);
+      listOfObjectsD.add(dog.animalsList.get(0));
+      listOfObjectsD.add(dog.animalsList.get(3));
+      cat.fodderCounter(cat.nameC, i+1, cat.weightFactorC, cat.ageFactorC, cat.kittyFactor, cat.kittyPortionFactor, cat.adultPortionFactorC);
+      listOfObjectsC.add(cat.animalsList.get(0));
+      listOfObjectsC.add(cat.animalsList.get(3));
 
-      dog.printer(dog.animalsList, dog.name, i+1);
-      cat.printer(cat.animalsList, cat.nameC, i+1);
+      dog.printer(dog.animalsList);
+      cat.printer(cat.animalsList);
       System.out.println();
       }
       
@@ -157,6 +153,8 @@ class laba2
 	{
     SearchInfo info = new SearchInfo();
     info.groupOfObjects();
+    info.printerInfo(info.listOfObjectsD);
+    info.printerInfo(info.listOfObjectsC);
     /*for (int i = 0; i < 100; i++) {
    
       Dog dog = new Dog();
